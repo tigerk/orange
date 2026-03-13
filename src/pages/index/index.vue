0a79dfb60196e21bded92bc66c6a8928d5188df4
tigerk <kimhwawoon@gmail.com>
2026-03-13 08:31:28 +0800

    Update project configuration and add component types declaration

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="index">
     <nut-button type="primary" @click="onClick">按钮</nut-button>
+    呵呵呵呵呵额和
     <nut-toast v-model:visible="show" msg="你成功了" />
   </view>
 </template>
